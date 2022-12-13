00fae9b3f71881c29c69f8a27d8d7d5cafd709d2
stan5315 <nwoforstanley@Gmail.com>
2022-12-13 07:07:59 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
