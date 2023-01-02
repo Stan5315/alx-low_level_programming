082c631f7bbb9863ed1cdbabbcb00a9393d155af
stan5315 <nwoforstanley@Gmail.com>
2023-01-02 08:39:17 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 #include "stdio.h"
 int _putchar(char c);
 
-char *_memset(char *c, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *destination, char *mem, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
